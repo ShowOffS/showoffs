4e2573538b01b584ecdb95358d5cd60337077573
adiktz <gauravraviji@gmail.com>
2016-02-23 18:26:35 +0530

    Implemented collapsing toolbar

--- app/src/main/java/in/showoffs/showoffs/activities/Dashboard.java
+++ app/src/main/java/in/showoffs/showoffs/activities/Dashboard.java
@@ -1,42 +1,107 @@
 package in.showoffs.showoffs.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import in.showoffs.showoffs.R;
 
-public class Dashboard extends BaseActivity {
+public class Dashboard extends BaseActivity implements AppBarLayout.OnOffsetChangedListener{
 
     private AccountHeader headerResult;
     private Drawer result;
+    private ProfileTracker profileTracker;
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+
+    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    private LinearLayout mTitleContainer;
+    private TextView mTitle;
+    private AppBarLayout mAppBarLayout;
+
+    @Bind(R.id.profilePictureView)
+    CircleImageView profilePictureView;
+    @Bind(R.id.main_imageview_placeholder)
+    ImageView backdrop;
+    @Bind(R.id.main_username)
+    TextView username;
+    @Bind(R.id.main_status)
+    TextView status;
+    Toolbar toolbar;
+    /*@Bind(R.id.appbar)
+    AppBarLayout appBar;
+
+    @Bind(R.id.rootLayout)
+    CoordinatorLayout rootLayout;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mTitle          = (TextView) findViewById(R.id.main_textview_title);
+        mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
+        mAppBarLayout   = (AppBarLayout) findViewById(R.id.main_appbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        toolbar.setTitle("");
+        mAppBarLayout.addOnOffsetChangedListener(this);
+
+        setSupportActionBar(toolbar);
+        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
-        /*headerResult = new AccountHeaderBuilder()
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                updateUI();
+                // It's possible that we were waiting for Profile to be populated in order to
+                // post a status update.
+            //    handlePendingAction();
+            }
+        };
+
+//        setUpDrawer(savedInstanceState);
+    }
+
+    private void setUpDrawer(Bundle savedInstanceState) {
+        headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withCompactStyle(false)
-        //        .withHeaderBackground(R.drawable.splash)
+                        //        .withHeaderBackground(R.drawable.splash)
                 .withSavedInstance(savedInstanceState)
                 .build();
 
@@ -54,7 +119,29 @@ public class Dashboard extends BaseActivity {
                         new SecondaryDrawerItem().withName("Contact").withIcon(FontAwesome.Icon.faw_bullhorn)
                 )
                 .withSavedInstance(savedInstanceState)
-                .build();*/
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+    private void updateUI() {
+        Profile profile = Profile.getCurrentProfile();
+        if (profile != null) {
+            Picasso.with(this).load(profile.getProfilePictureUri(200,200)).into(profilePictureView);
+            Picasso.with(this).load(profile.getProfilePictureUri(250,250)).into(backdrop);
+            username.setText(profile.getName());
+            status.setText(profile.getLinkUri().toString());
+        }
     }
 
     @Override
@@ -78,4 +165,65 @@ public class Dashboard extends BaseActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if(mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+    public static void startAlphaAnimation (View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
+    }
+
+    /*public  void expandToolbar(Bitmap bmp) {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appBar.getLayoutParams();
+        AppBarLayout.Behavior behavior = new AppBarLayout.Behavior();
+        behavior.setTopAndBottomOffset(0);
+        behavior.onNestedPreScroll(rootLayout, appBar, null, 0, bmp.getHeight() - 512, new int[2]);
+        params.setBehavior(behavior);
+        appBar.setLayoutParams(params);
+    }*/
 }
